fd4e3a22cb2e246e0b11973db9e59c7526e4e31b
Rangga Adithia <rangga.adithia26@gmail.com>
2025-07-29 21:46:37 +0800

    implement repository pattern

--- src/main/java/id/soc/constraints/UniqueEmailValidator.java
+++ src/main/java/id/soc/constraints/UniqueEmailValidator.java
@@ -1,14 +1,18 @@
 package id.soc.constraints;
 
-import id.soc.entity.User;
+import id.soc.repository.UserRepository;
+import jakarta.inject.Inject;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
 
+    @Inject
+    UserRepository userRepository;
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        return User.find("email", value).firstResult() == null;
+        return userRepository.find("email", value).firstResult() == null;
     }
 
 }
